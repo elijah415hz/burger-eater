734d3babb2090fcb6319a0f29c3186196b838230
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-10-14 14:50:31 -0700

    Database schema and mysql.js connection

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, eaten) 
+    VALUES ("Cheeseburger", false),
+        ("Bacon Cheeseburger", false),
+        ("Avocado Burger", false),
+        ("BBQ Burger", false)
