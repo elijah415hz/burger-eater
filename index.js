3b9ee2a32521b9428e3100d1f4b3c7b7c258e950
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-10-15 15:28:46 -0700

    Styling

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ app.use(express.json());
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main", helpers: {
+    plusOne: function (value, options) {
+      return parseInt(value) + 1;
+    }
+  }
+}));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
@@ -23,7 +29,7 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
--- public/assets/js/burger_script.js
+++ public/assets/js/burger_script.js
@@ -15,19 +15,23 @@ $(function () {
         console.log(id);
         $.ajax("/api/burgers/" + id, {
             method: "PUT",
-            data: {eaten: false}
-        }).then(function() {
+            data: { eaten: false }
+        }).then(function () {
             location.reload();
         });
     });
-    $(".create-form").on("submit", function(event) {
+    $(".create-form").on("submit", function (event) {
         event.preventDefault();
         const burgerName = $("#burger-name").val().trim();
         $.ajax("/api/burgers/", {
             method: "POST",
-            data: {burger_name: burgerName}
-        }).then(function() {
+            data: { burger_name: burgerName }
+        }).then(function () {
             location.reload();
         })
     })
+    const Handlebars = require('handlebars');
+    Handlebars.registerHelper("plusOne", function (value, options) {
+        return parseInt(value) + 1;
+    });
 });
