c3371103203ce875d941f88a9c0e4a5a3ea3658a
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-10-14 19:46:31 -0700

    make-burger buttons working

--- public/assets/js/burger_script.js
+++ public/assets/js/burger_script.js
@@ -1,14 +1,25 @@
-$(function() {
-    $(".eat-burger").on("click", function(event) {
-    const id = $(this).data("id")
-    console.log(id)
-    $.ajax("/api/burgers/" + id, {
-        method: "PUT",
-        data: {eaten: true}
-    }).then(function() {
-        location.reload();
-    })
+$(function () {
+    $(".eat-burger").on("click", function () {
+        const id = $(this).data("id");
+        console.log(id);
+        $.ajax("/api/burgers/" + id, {
+            method: "PUT",
+            data: { eaten: true }
+        }).then(function () {
+            location.reload();
+        });
 
-});
+    });
+    $(".make-burger").on("click", function () {
+        const id = $(this).data("id");
+        console.log(id);
+        $.ajax("/api/burgers/" + id, {
+            method: "PUT",
+            data: {eaten: false}
+        }).then(function() {
+            location.reload();
+        });
+    });
+    
 
 });
